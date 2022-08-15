8eaefadc7c23dd1d4e8143c0e3492ee5c10b8bc6
gabripo <gabripo93+github@gmail.com>
2022-08-15 11:20:21 +0200

    Create MaxComparator class

---
+++ MaxComparator/MaxComparator.cpp
@@ -0,0 +1,6 @@
+#include "MaxComparator.h"
+
+template<typename T>
+bool MaxComparator<T>::prior(T a, T b) {
+    return (a > b);
+}
---
+++ MaxComparator/MaxComparator.h
@@ -0,0 +1,5 @@
+template<typename T>
+class MaxComparator {
+    public:
+        static bool prior(T a, T b);
+};
